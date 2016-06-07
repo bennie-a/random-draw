0f33d4d64c5eeb803f33dba349fe8a041389b84b
ベニヤマアイコ <bennie.a.320@gmail.com>
2016-06-07 19:49:26 +0900

    左袖レイアウトに変更。

--- index.php
+++ index.php
@@ -12,10 +12,11 @@ session_start();
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="css/base.css" rel="stylesheet" type="text/css">
 <link href="css/position.css" rel="stylesheet" type="text/css">
+<link href="css/layout.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-	<header class="navbar navbar-inverse navbar-fixed-top">
+	<!-- <header class="navbar navbar-inverse navbar-fixed-top">
 		<div class="col-sm-11">
 			<div class="navbar-header">
 				<a href="/" class="navbar-brand">お絵かき出題アプリ</a>
@@ -26,10 +27,11 @@ session_start();
 				</ul>
 			</div>
 		</div>
-	</header>
+	</header> -->
 <div class="container-fluid">
 	<div class="row-fluid contents">
-		<div class="sidemenu col-sm-2">
+		<div class="sidemenu col-md-2">
+			<a class="logo" href="#">お絵かき出題アプリ</a>
 			<nav class="sidebar-nav" role="complementary">
 				<figure>
 						<img src="member_picture/<?php echo $_SESSION['icon']; ?>" class="icon" />
@@ -37,12 +39,15 @@ session_start();
 				</figure>
 				<ul class="nav nav-pills nav-stacked">
 					<li><a href="post/index.php">絵を描く</a></li>
-					<li><a href="#">お絵かき一覧</a></li>
+					<li><a href="#">みんなのお絵かき</a></li>
 					<li><a href="#">アップロード履歴</a></li>
 				</ul>
+				<div class="btn_area">
+					<a href="#" class="btn btn-default btn-sm">ログアウト</a>
+				</div>
 			</nav>
 		</div>
-			<main class="col-sm-10">
+			<main class="col-md-10" role="main">
 				<article id="start" class="jumbotron row">
 					<p class="lead">紙と鉛筆の用意はできましたか？ではさっそく始めましょう！</p>
 					<a href="post/index.php" class="btn btn-danger btn-lg">絵を描く</a>
@@ -97,6 +102,7 @@ session_start();
 				            </div>
 										<div class="panel-footer">パネルの見出し</div>
 				        </div>
+								<a href="#" class="btn btn-default btn-lg">もっと見る→</a>
 					</article>
 				</div>
 			</main>
